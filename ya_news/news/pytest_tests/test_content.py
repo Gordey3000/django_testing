31f19dc4544235994f381750fbb13eba783880e7
Гордей Савичев <gordey159@gmail.com>
2023-08-08 17:08:36 +0300

    second rewie 3

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -20,15 +20,14 @@ def test_news_order(all_news, client, url_home):
     assert all_dates == sorted_dates
 
 
-def test_comments_order(client, news, created_comment, url_detail):
+@pytest.mark.usefixtures('created_comment')
+def test_comments_order(client, url_detail):
     response = client.get(url_detail)
-    news = response.context['news']
-    all_set_comments = [
-        created_comment.created for created_comment in news.comment_set.all()
-    ]
-    news.comment_set.all()
-    sorted_comments = sorted(all_set_comments)
-    assert all_set_comments == sorted_comments
+    object_list_comments = list(response.context['news'].comment_set.all())
+    sorted_comments = sorted(
+        object_list_comments, key=lambda comment: comment.created
+    )
+    assert object_list_comments == sorted_comments
 
 
 def test_anonymous_client_has_no_form(news, client, url_detail):
